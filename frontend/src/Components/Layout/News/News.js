b590926baf345b0b17c6dddca8c529180262b4a3
Emmanuel Perrin Joseph <ejoseph3@masonlive.gmu.edu>
2021-05-07 03:50:51 -0400

    creates news card and news side bar

--- frontend/src/Components/Layout/News/News.js
+++ frontend/src/Components/Layout/News/News.js
@@ -1,9 +1,14 @@
 import React from "react";
 import NewsCard from "../../Shared/NewsCard/NewsCard";
+import SearchIcon from "@material-ui/icons/Search";
 
 function News() {
   return (
     <section className="news">
+      <div className="news__top">
+        <SearchIcon />
+        <input className= "news__search"type="text" placeholder="Search twitter"></input>
+      </div>
       <NewsCard />
     </section>
   );
--- frontend/src/Components/Shared/NewsCard/NewsCard.js
+++ frontend/src/Components/Shared/NewsCard/NewsCard.js
@@ -3,7 +3,53 @@ import React from "react";
 function NewsCard() {
   return (
     <div className="newsCard">
-
+      <div className="newsCard__container">
+        <header>
+          <h1>What's happening</h1>
+        </header>
+      </div>
+      <div className="newsCard__container">
+        <header className="newsCard__info">Trending in United States</header>
+        <main className="newsCard__info">Candis</main>
+        <small className="newsCard__info sidebar__avatar__username">
+          candice 11.7K Tweets
+        </small>
+      </div>
+      <div className="newsCard__container">
+        <header className="newsCard__info">Trending in United States</header>
+        <main className="newsCard__info">Candis</main>
+        <small className="newsCard__info sidebar__avatar__username">
+          candice 11.7K Tweets
+        </small>
+      </div>
+      <div className="newsCard__container">
+        <header className="newsCard__info">Trending in United States</header>
+        <main className="newsCard__info">Candis</main>
+        <small className="newsCard__info sidebar__avatar__username">
+          candice 11.7K Tweets
+        </small>
+      </div>
+      <div className="newsCard__container">
+        <header className="newsCard__info">Trending in United States</header>
+        <main className="newsCard__info">Candis</main>
+        <small className="newsCard__info sidebar__avatar__username">
+          candice 11.7K Tweets
+        </small>
+      </div>
+      <div className="newsCard__container">
+        <header className="newsCard__info">Trending in United States</header>
+        <main className="newsCard__info">Candis</main>
+        <small className="newsCard__info sidebar__avatar__username">
+          candice 11.7K Tweets
+        </small>
+      </div>
+      <div className="newsCard__container">
+        <header className="newsCard__info">Trending in United States</header>
+        <main className="newsCard__info">Candis</main>
+        <small className="newsCard__info sidebar__avatar__username">
+          candice 11.7K Tweets
+        </small>
+      </div>
     </div>
   );
 }
